3463cbb402a40713e1cca9dffb20ba279704cc1c
Struck713 <noahstruck71303@gmail.com>
2024-12-08 01:55:41 -0500

    Tokenize string data types as a whole

--- tokenizer.go
+++ tokenizer.go
@@ -38,6 +38,15 @@ func Tokenize(lines []string) []Token {
 				tokens = append(tokens, NewToken(string('\n'), y, x))
 				x = len(line)
 				break
+			case '"':
+				x++
+				start = x
+				for x < len(line) && line[x] != '"' {
+					x += 1
+				}
+				tokens = append(tokens, NewToken(string(line[start:x]), y, x))
+				start = x + 1
+				break
 			case ',', ':', '\n':
 				if start < x {
 					tokens = append(tokens, NewToken(line[start:x], y, start))
